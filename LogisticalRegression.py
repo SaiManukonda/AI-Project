f448b36d82cecfd8434a17f63a935cd8a97d968e
arjunUpatel <arjunpatel151002@gmail.com>
2023-12-15 21:42:45 -0500

    code dump

--- LogisticalRegression.py
+++ LogisticalRegression.py
@@ -1,48 +1,58 @@
 import numpy as np
 import pandas as pd
+import random as rand
+
 
 class LogisticalRegression:
-    #know it says task1 but this is abstract logistical regression 
-    #X is the input space
-    #X has to be a pandas dataframe of the input matrix
-    #Y has to be a pandas dataframe of the input matrix
+    # know it says task1 but this is abstract logistical regression
+    # X is the input space
+    # X has to be a pandas dataframe of the input matrix
+    # Y has to be a pandas dataframe of the input matrix
     def __init__(self, X, Y, learningRate, iterations):
         self.X = X
         self.Y = Y
         self.learningRate = learningRate
         self.iterations = iterations
-    
-    #trains the data and once the training is done, it returns W and B, which can be used
-    #to predict new images
+
+    # trains the data and once the training is done, it returns W and B, which can be used
+    # to predict new images
     def train(self):
-        m = self.X.shape[1]
-        n = self.X.shape[0]
-        
-        W = np.zeros((n, 1))
+        m = self.X.shape[0]
+        n = self.X.shape[1]
+
+        W = np.zeros((m, 1))
         B = 0
-        print(W.shape)
-        
+        print(self.X[0].shape)
+        picked = set()
         for i in range(self.iterations):
+            rand_data_idx = rand.randint(0, m - 1)
+            while rand_data_idx in picked:
+                rand_data_idx = rand.randint(0, m - 1)
+            picked.add(rand_data_idx)
             print(W.T.shape)
             print(self.X.shape)
-            Z = np.dot(W.T, self.X) + B
-            print("1")
-            #sigmoid function
-            A = 1/(1+np.exp(-1*Z))
-            
-            cost = -(1/m)*np.sum(self.Y*np.log(A) + (1-self.Y)*np.log(1-A))
-            
-            dw = (1/m)*np.dot(A-self.Y, self.X.T)
-            db = (1/m)*np.sum(A - self.Y)
-            
+            Z = np.dot(W, self.X[rand_data_idx]) + B
+            print(Z)
+            # sigmoid function
+            A = 1 / (1 + np.exp(-1 * Z))
+
+            print(A)
+
+            for i in range(1, n + 1):
+                cost += (self.Y.iloc(i) * np.log(A)) + (
+                    1 - self.Y.iloc(i) * np.log(1 - A)
+                )
+                cost = cost(1 / n) * -1
+
+            # cost = -(1 / m) * np.sum(self.Y * np.log(A) + (1 - self.Y) * np.log(1 - A))
+
+            dw = (1 / n) * np.dot(A - self.Y, self.X.T)
+            db = (1 / n) * np.sum(A - self.Y)
+
             W = W - self.learningRate * dw.T
             B = B - self.learningRate * db
-            
-            if(i%(self.iterations/10) == 0):
+
+            if i % (self.iterations / 10) == 0:
                 print("Cost = " + str(cost))
 
         return W, B
-        
-            
-        
-        
--- Task1.py
+++ Task1.py
@@ -3,69 +3,69 @@ import numpy as np
 import pandas as pd
 from LogisticalRegression import LogisticalRegression
 
-#creating a data set of 1000 inputs and outputs
+# creating a data set of 1000 inputs and outputs
 X = []
 Y = []
 for i in range(1000):
     currentWiring = WiringDiagram()
     flattenedFeatureArray = []
-    #matrix of vector representation of the colors
+    # matrix of vector representation of the colors
     mat = []
     for i in range(len(currentWiring.diagram)):
         for f in range(len(currentWiring.diagram)):
-            if currentWiring.diagram[i,f] == 0:
-                mat.append([0,0,0,0])
+            if currentWiring.diagram[i, f] == 0:
+                mat.append([0, 0, 0, 0])
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
-            elif currentWiring.diagram[i,f] == 1:
-                mat.append([1,0,0,0])
+            elif currentWiring.diagram[i, f] == 1:
+                mat.append([1, 0, 0, 0])
                 flattenedFeatureArray.append(1)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
-            elif currentWiring.diagram[i,f] == 2:
-                mat.append([0,1,0,0])
+            elif currentWiring.diagram[i, f] == 2:
+                mat.append([0, 1, 0, 0])
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(1)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
-            elif currentWiring.diagram[i,f] == 3:
-                mat.append([0,0,1,0])
+            elif currentWiring.diagram[i, f] == 3:
+                mat.append([0, 0, 1, 0])
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(1)
                 flattenedFeatureArray.append(0)
             else:
-                mat.append([0,0,0,1])
+                mat.append([0, 0, 0, 1])
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(0)
                 flattenedFeatureArray.append(1)
     for i in range(len(mat)):
-        if i%2 == 0:
-           firstMatrix = np.array(mat[i]) 
-           secondMatrix = np.array(mat[i+1])
-           flattenedFeatureArray.append(np.dot(firstMatrix, secondMatrix))
-    mat = []       
+        if i % 2 == 0:
+            firstMatrix = np.array(mat[i])
+            secondMatrix = np.array(mat[i + 1])
+            flattenedFeatureArray.append(np.dot(firstMatrix, secondMatrix))
+    mat = []
     for i in range(len(currentWiring.diagram)):
         for f in range(len(currentWiring.diagram)):
-            if currentWiring.diagram[f,i] == 0:
-                mat.append([0,0,0,0])
-            elif currentWiring.diagram[f,i] == 1:
-                mat.append([1,0,0,0])
-            elif currentWiring.diagram[f,i] == 2:
-                mat.append([0,1,0,0])
-            elif currentWiring.diagram[f,i] == 3:
-                mat.append([0,0,1,0])
+            if currentWiring.diagram[f, i] == 0:
+                mat.append([0, 0, 0, 0])
+            elif currentWiring.diagram[f, i] == 1:
+                mat.append([1, 0, 0, 0])
+            elif currentWiring.diagram[f, i] == 2:
+                mat.append([0, 1, 0, 0])
+            elif currentWiring.diagram[f, i] == 3:
+                mat.append([0, 0, 1, 0])
             else:
-                mat.append([0,0,0,1])
+                mat.append([0, 0, 0, 1])
     for i in range(len(mat)):
-        if i%2 == 0:
-           firstMatrix = np.array(mat[i]) 
-           secondMatrix = np.array(mat[i+1])
-           flattenedFeatureArray.append(np.dot(firstMatrix, secondMatrix))
+        if i % 2 == 0:
+            firstMatrix = np.array(mat[i])
+            secondMatrix = np.array(mat[i + 1])
+            flattenedFeatureArray.append(np.dot(firstMatrix, secondMatrix))
     X.append(flattenedFeatureArray)
     Y.append(currentWiring.dangerous)
 X = np.array(X)
@@ -75,5 +75,3 @@ Y_Train = pd.DataFrame(Y).T
 
 Train_1000 = LogisticalRegression(X_Train, Y_Train, 0.0005, 10000)
 print(Train_1000.train())
-    
-
